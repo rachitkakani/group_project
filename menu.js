ba9150afb49a12f64e7559f7a24f887da8b744a1
Priyanshu23u <upriyanshujnp@gmail.com>
2023-11-05 19:45:45 +0530

    Added menu page for breakfast

---
+++ menu.js
@@ -0,0 +1,13 @@
+function openslidemenu(){
+    document.getElementById("menu").style.width="250px";
+    document.getElementById("content").style.marginLeft="250px";
+    
+    }
+    function closeslidemenu(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("content").style.marginLeft="0px";
+    }
+    
+    
+    
+    window.alert("Welcome to Restaurant");
