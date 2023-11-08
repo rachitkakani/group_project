d3bc1f060123ab1d70ecf50cc5969ce540f51db5
Astha-Pathak <asthapathak684@gmail.com>
2023-11-08 12:32:26 +0530

    Rate

---
+++ script.js
@@ -0,0 +1,50 @@
+// Get all star elements
+const stars = document.querySelectorAll(".star");
+
+// Get rating value and display it
+const ratingValue = document.querySelector(".rating-value");
+
+stars.forEach((star) => {
+    star.addEventListener("click", () => {
+        const rating = star.getAttribute("data-rating");
+        ratingValue.textContent = rating;
+        highlightStars(+rating);
+    });
+});
+
+function highlightStars(rating) {
+    stars.forEach((star) => {
+        const starRating = star.getAttribute("data-rating");
+        if (starRating <= rating) {
+            star.style.transform = "scale(1.1)";
+        } else {
+            star.style.transform = "scale(1)";
+        }
+    }
+}
+
+// Submit review
+const submitButton = document.querySelector(".submit-review");
+const userComment = document.querySelector(".user-comment");
+const userReviews = document.querySelector(".user-review");
+
+submitButton.addEventListener("click", () => {
+    const rating = ratingValue.textContent;
+    const comment = userComment.value;
+
+    if (comment) {
+        const review = document.createElement("div");
+        review.innerHTML = `<p><strong>Rating: ${rating}/5</strong></p><p>${comment}</p>`;
+        userReviews.appendChild(review);
+        userComment.value = "";
+        ratingValue.textContent = "0";
+        resetStars();
+    }
+});
+
+function resetStars() {
+    stars.forEach((star) => {
+        star.style.transform = "scale(1)";
+    });
+}
+
